a5a069bda2e1d6b5d9e8ba899ff89af08f92c2ca
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2021-01-20 12:28:04 +0100

    Simple Ui and Mouse Tracking added.

---
+++ Main.py
@@ -0,0 +1,47 @@
+'''
+Created on 20 jan. 2021
+
+@author: edu
+'''
+from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSignal
+from MausClickerUI import Ui_MainWindow
+from PyQt5.QtWidgets import QMainWindow
+from MouseTracking import MouseTracking
+import sys
+
+
+class MainControl(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        # Creación MouseTracking class
+        self.mouse = MouseTracking()
+        self.mouse.start()
+        self.mouse.my_signal.connect(self.mouseGetPos)
+
+        # Creacion objetos ventana grafica
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.initialization()
+
+    def initialization(self):
+
+        # Botones
+        self.ui.stopBtn.setEnabled(False)
+        self.ui.playBtn.setEnabled(False)
+        return
+
+    def mouseGetPos(self,x,y):
+        posX = x
+        posY = y
+        self.ui.coordLbl.setText(f"Coords: {x}, {y}")
+
+
+app = QtWidgets.QApplication(sys.argv)
+mainWindow = MainControl()
+mainWindow.show()
+sys.exit(app.exec_())
+
+
---
+++ MouseTracking.py
@@ -0,0 +1,50 @@
+'''
+Created on 20 jan. 2021
+
+@author: edu
+'''
+from PyQt5.QtCore import QThread,pyqtSignal
+from pynput.mouse import Controller
+
+
+class MouseTracking(QThread):
+    my_signal = pyqtSignal('PyQt_PyObject','PyQt_PyObject')
+    def __init__(self):
+        QThread.__init__(self)
+        self.mouse = Controller()
+        self.mousePos = self.mouse.position
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        while(True):
+            if self.mousePos != self.mouse.position:
+                self.mousePos = self.mouse.position
+                self.my_signal.emit(self.mousePos[0],self.mousePos[1])
+
+    '''
+    mouse = Controller()
+
+    # Read pointer position
+    print('The current pointer position is {0}'.format(
+        mouse.position))
+
+    # Set pointer position
+    mouse.position = (10, 20)
+    print('Now we have moved it to {0}'.format(
+        mouse.position))
+
+    # Move pointer relative to current position
+    mouse.move(5, -5)
+
+    # Press and release
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+
+    # Double click; this is different from pressing and releasing
+    # twice on macOS
+    mouse.click(Button.left, 2)
+
+    # Scroll two steps down
+    mouse.scroll(0, 2)
+    '''
