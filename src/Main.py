d04b5ff390363769d11b490b5ec2e99541905a20
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2021-01-26 12:23:28 +0100

    Changes on UI, Now you can add your coords on the Table and play the script.

--- src/Main.py
+++ src/Main.py
@@ -4,11 +4,12 @@ Created on 20 jan. 2021
 @author: edu
 '''
 from PyQt5 import QtWidgets
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import pyqtSignal
+from PyQt5 import QtCore
 from MausClickerUI import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow
 from MouseTracking import MouseTracking
+from pynput.mouse import Controller, Button
+import time
 import sys
 
 
@@ -16,9 +17,10 @@ class MainControl(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         # Creación MouseTracking class
-        self.mouse = MouseTracking()
-        self.mouse.start()
-        self.mouse.my_signal.connect(self.mouseGetPos)
+        self.mouseTrack = MouseTracking()
+        self.mouseTrack.start()
+        # Señal del Thread que llamará el método MouseGetPos
+        self.mouseTrack.my_signal.connect(self.mouseGetPos)
 
         # Creacion objetos ventana grafica
         self.ui = Ui_MainWindow()
@@ -31,15 +33,74 @@ class MainControl(QMainWindow):
         # Botones
         self.ui.stopBtn.setEnabled(False)
         self.ui.playBtn.setEnabled(False)
+
+        if self.ui.coordList.rowCount() > 0:
+            self.ui.playBtn.setEnabled(True)
+        self.ui.playBtn.clicked.connect(self.startScript)
+
+        # Header de la tabla
+        self.ui.coordList.setHorizontalHeaderLabels(("Coord X", "Coord Y", "Delay"))
         return
 
     def mouseGetPos(self,x,y):
+        ''' Método donde a traves del Thread MouseTracking obtenemos las coordenadas
+        del ratón y las asignamos al label para que se vea en tiempo real.'''
+        self.x = x
+        self.y = y
         self.ui.coordLbl.setText(f"Coords: {x}, {y}")
 
+    def keyPressEvent(self, qKeyEvent):
+        if qKeyEvent.key() == QtCore.Qt.Key_F7:
+            self.ui.playBtn.setEnabled(True)
+            '''Al pulsar F7 añadimos las coordenadas a la tabla'''
+            numRows = self.ui.coordList.rowCount() #Contamos las columnas que hay
+            self.ui.coordList.insertRow(numRows) #Añadimos una nueva columna en función de las que haya
+
+            # Añadimos coordenadas X, Y y un delay fijo
+            self.ui.coordList.setItem(numRows, 0, QtWidgets.QTableWidgetItem(str(self.x)))
+            self.ui.coordList.setItem(numRows, 1, QtWidgets.QTableWidgetItem(str(self.y)))
+            self.ui.coordList.setItem(numRows, 2, QtWidgets.QTableWidgetItem("2000"))
+
+            # Centramos los datos
+            self.ui.coordList.item(numRows,0).setTextAlignment(QtCore.Qt.AlignCenter)
+            self.ui.coordList.item(numRows, 1).setTextAlignment(QtCore.Qt.AlignCenter)
+            self.ui.coordList.item(numRows, 2).setTextAlignment(QtCore.Qt.AlignCenter)
+
+        if qKeyEvent.key() == QtCore.Qt.Key_F6:
+            self.ui.stopBtn.setEnabled(True)
+            numRows = self.ui.coordList.rowCount()  # Contamos las columnas que hay
+            mouse = Controller() #Creamos un controller del raton
+            for row in range(numRows):
+                # Por cada columna existente cogemos los valores y movemos el ratón a las posiciones con su delay.
+                coordX = self.ui.coordList.item(row,0)
+                coordY = self.ui.coordList.item(row, 1)
+                delay = self.ui.coordList.item(row, 2)
+                time.sleep(int(delay.text())/1000)
+                mouse.position = (int(coordX.text()),int(coordY.text()))
+                mouse.click(Button.left, 1)
+    def startScript(self):
+        #TODO THREAD
+        self.ui.stopBtn.setEnabled(True)
+        numRows = self.ui.coordList.rowCount()  # Contamos las columnas que hay
+        mouse = Controller()  # Creamos un controller del raton
+        for row in range(numRows):
+            # Por cada columna existente cogemos los valores y movemos el ratón a las posiciones con su delay.
+            coordX = self.ui.coordList.item(row, 0)
+            coordY = self.ui.coordList.item(row, 1)
+            delay = self.ui.coordList.item(row, 2)
+            time.sleep(int(delay.text()) / 1000)
+            mouse.position = (int(coordX.text()), int(coordY.text()))
+            mouse.click(Button.left, 1)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 app = QtWidgets.QApplication(sys.argv)
 mainWindow = MainControl()
 mainWindow.show()
+sys.excepthook = except_hook
 sys.exit(app.exec_())
+
 
 
--- src/MouseTracking.py
+++ src/MouseTracking.py
@@ -11,6 +11,7 @@ class MouseTracking(QThread):
     my_signal = pyqtSignal('PyQt_PyObject','PyQt_PyObject')
     def __init__(self):
         QThread.__init__(self)
+        #Creamos un objeto Controller que nos permitirá trackear el ratón
         self.mouse = Controller()
         self.mousePos = self.mouse.position
     def __del__(self):
@@ -18,6 +19,7 @@ class MouseTracking(QThread):
 
     def run(self):
         while(True):
+            # Mientras el Thread esté activo y la posición del ratón sea diferente se guardará la nueva posición y se mandará como señal.
             if self.mousePos != self.mouse.position:
                 self.mousePos = self.mouse.position
                 self.my_signal.emit(self.mousePos[0],self.mousePos[1])
